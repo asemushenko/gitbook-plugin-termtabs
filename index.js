834a1a91e0b802d2aeca19ad4a356133ec012e3c
Alena Semushenko <asemushenko@wallarm.com>
2019-07-09 18:12:51 +0700

    Remove configs

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ function createTab(block, i, isActive) {
     return '<div class="tab' + (isActive? ' active' : '') + '" data-termtab="' + i + '">' + block.kwargs.name + '</div>';
 }
 
-function processLine(line, config) {
+function processLine(line) {
     var str = "";
     var commands = "";
 
-    re = XRegExp(config.prompt);
+    re = XRegExp(properties.prompt.default);
 
     if(re.test(line)) {
         var parts = XRegExp.exec(line, re);
@@ -30,17 +30,16 @@ function processLine(line, config) {
         }
         str += '\n';
 
-        if(config.copyButtons) {
-            if(!parts.hasOwnProperty('command')) {
-                throw Error("Cannot use copyButtons if command does not exist as a named group in the prompt regexp!");
-            }
-            commands += parts.command.replace(/"/g,'&quot;') + '\n';
+        if(!parts.hasOwnProperty('command')) {
+            throw Error("Cannot use copyButtons if command does not exist as a named group in the prompt regexp!");
         }
+        commands += parts.command.replace(/"/g,'&quot;') + '\n';
+
     } else {
-        if(config.lineStyles && line.startsWith("[warning]")) {
+        if(line.startsWith("[warning]")) {
             str += '<span class="t-line t-warning">';
             str += line.replace("[warning]","") + '\n';
-        } else if (config.lineStyles && line.startsWith("[error]")) {
+        } else if (line.startsWith("[error]")) {
             str += '<span class="t-line t-error">';
             str += line.replace("[error]","") + '\n';ч
         } else {
@@ -54,19 +53,19 @@ function processLine(line, config) {
     return {"line":str, "command":commands};
 }
 
-function processCode(thebody, config, isweb) {
-    var str =  `<pre class="term t-${config.style}${config.fade ? ' t-fade' : ''}">`;
+function processCode(thebody, isweb) {
+    var str =  `<pre class="term t-default t-fade">`;
     var body = thebody.trim().split('\n');
     var lines = "";
     var commands = ""
     for(i=0; i<body.length; i++) {
-        var tmp = processLine(body[i], config);
+        var tmp = processLine(body[i]);
         lines += tmp.line;
         commands += tmp.command;
     }
 
     /* JSON backend (README) should not have buttons (it does not have js or styles) */
-    if(isweb && config.copyButtons && commands != "") {
+    if(isweb && commands != "") {
         str += `<button class="btn t-copy" data-clipboard-text="${commands.trim()}"><svg class="octicon octicon-clippy" viewBox="0 0 14 16" version="1.1" width="14" height="14" aria-hidden="true"><path fill-rule="evenodd" d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"></path></svg></button>`
     }
 
@@ -82,11 +81,10 @@ function processCode(thebody, config, isweb) {
     @return {String}
 */
 function createTabBody(block, i, isActive) {
-    var opts   = this.config.get('pluginsConfig.term');
     var isweb = this.output.name == 'website';
 
     return '<div class="tab' + (isActive? ' active' : '') + '" data-termtab="' + i + '">'
-        + processCode(block.body, opts, isweb).body + '</div>';
+        + processCode(block.body, isweb).body + '</div>';
 }
 
 module.exports = {
