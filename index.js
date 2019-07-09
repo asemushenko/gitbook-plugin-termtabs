9d1eb34730877c6682b267167f65359ad96d8521
Alena Semushenko <asemushenko@wallarm.com>
2019-07-09 20:01:11 +0700

    Add buttons and increment version

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function processLine(line) {
     return {"line":str, "command":commands};
 }
 
-function processCode(thebody, isweb) {
+function processCode(thebody) {
     var str =  `<pre class="term t-default t-fade">`;
     var body = thebody.trim().split('\n');
     var lines = "";
@@ -65,9 +65,7 @@ function processCode(thebody, isweb) {
     }
 
     /* JSON backend (README) should not have buttons (it does not have js or styles) */
-    if(isweb && commands != "") {
-        str += `<button class="btn t-copy" data-clipboard-text="${commands.trim()}"><svg class="octicon octicon-clippy" viewBox="0 0 14 16" version="1.1" width="14" height="14" aria-hidden="true"><path fill-rule="evenodd" d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"></path></svg></button>`
-    }
+    str += `<button class="btn t-copy" data-clipboard-text="${commands.trim()}"><svg class="octicon octicon-clippy" viewBox="0 0 14 16" version="1.1" width="14" height="14" aria-hidden="true"><path fill-rule="evenodd" d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"></path></svg></button>`
 
     str += lines + '</pre>';
     return {body:str, html:true};
@@ -82,7 +80,7 @@ function processCode(thebody, isweb) {
 */
 function createTabBody(block, i, isActive) {
     return '<div class="tab' + (isActive? ' active' : '') + '" data-termtab="' + i + '">'
-        + processCode(block.body, false).body + '</div>';
+        + processCode(block.body).body + '</div>';
 }
 
 module.exports = {
